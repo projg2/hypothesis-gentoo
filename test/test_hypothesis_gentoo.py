8a94effa4bfaa63b1596e06c944f133926425079
Michał Górny <mgorny@gentoo.org>
2025-07-05 07:57:07 +0200

    Also fix with --hypothesis-profile=gentoo used to call pytest
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- test/test_hypothesis_gentoo.py
+++ test/test_hypothesis_gentoo.py
@@ -17,7 +17,9 @@ def test_reject_most(i: int) -> None:
 
 def test_disabled(pytester: pytest.Pytester) -> None:
     pytester.makepyfile(TEST_CASE)
-    result = pytester.runpytest()
+    result = pytester.runpytest(
+        "-p", "hypothesispytest", "--hypothesis-profile=default"
+    )
     result.assert_outcomes(failed=1)
 
 
